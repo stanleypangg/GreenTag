e0faae6f3f8c9bae42200a7b0ae464d5c3d24b3b
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-03-05 22:57:51 -0500

    TailwindCSS uninstalled and ChakraUI installed

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Provider } from "@/components/ui/provider";
 
 export const metadata = {
   title: "Create Next App",
@@ -18,11 +8,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
--- client/app/page.js
+++ client/app/page.js
@@ -1,24 +1,27 @@
 "use client"
 
 import React, { useEffect, useState } from 'react'
+import { Slider } from '@chakra-ui/react';
 
 function page() {
   // message is initially Loading
   // Once data is retrieved
   // message = data.message
-  const [message, setMessage] = useState("Loading");
+  // const [message, setMessage] = useState("Loading");
 
-  useEffect(() => {
-    fetch("http://localhost:8080/scan-tag")
-     .then(response => response.json())
-     .then(data => {
-        setMessage(data.message);
-      }
-    );
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:8080/")
+  //    .then(response => response.json())
+  //    .then(data => {
+  //       setMessage(data.message);
+  //     }
+  //   );
+  // }, []);
 
   return (
-    <div>{message}</div>
+    <>
+      <div></div>
+    </>
   )
 }
 
